81fdffdd8f857cea05fc5e9f36cbd2d038e17b39
MuhammadHassanVahand <hassan12rehan@gmail.com>
2023-07-09 10:37:30 +0500

    update

--- fanction marksheet.dart
+++ fanction marksheet.dart
@@ -1,8 +1,8 @@
 void main() {
   print("Student1: ${markSheet(marks1: 70, marks2: 65, marks3: 64)}");
-  print("Student1: ${markSheet(marks1: 87, marks2: 55, marks3: 84)}");
-  print("Student1: ${markSheet(marks1: 600, marks2: 44, marks3: 44)}");
-  print("Student1: ${markSheet(marks1: 33, marks2: 55, marks3: 46)}");
+  print("Student2: ${markSheet(marks1: 87, marks2: 55, marks3: 84)}");
+  print("Student3: ${markSheet(marks1: 600, marks2: 44, marks3: 44)}");
+  print("Student4: ${markSheet(marks1: 33, marks2: 55, marks3: 46)}");
 }
 
 markSheet({marks1, marks2, marks3}) {
